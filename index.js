d55bcc3b8da4c687a5b64efeef6427fb4d430bb6
bouquet1 <bukuleta@gmail.com>
2023-03-11 14:42:09 -0800

    complete objects lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: "employeeNAme",
+    address: "employeeAddress",
+} 
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]= value;
+    return employee;
+}
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     delete employee[key];
+//     return employee;
+// }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    employee = {
+        key: "Sam"
+    }
+    delete employee[key];
+    return employee;
+}
